81c822951d0eb563d4207a422e1610bcd6efbac5
gabrielvqz1 <126103730+gabrielvqz1@users.noreply.github.com>
2023-03-29 13:58:45 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <title>mis datos</title>
+  </head>
+  <body bgcolor="yellow">
+
+<?php
+echo 'nombre:'.'edwin gabriel vazques flores';
+?>
+<br>
+<?php
+echo 'calle:'.'san javier';
+?>
+<br>
+<?php
+echo 'numero de casa:'.'8448485656';
+?>
+<br>
+<?php
+echo 'colonia:'.'las candelarias';
+?>
+<br>
+<?php
+echo 'municipio:'.'saltillo,  coahuila';
+?>
+<br>
+<?php
+echo 'telefono:'.'84423232323';
+?>
+  </body>
+</html>
